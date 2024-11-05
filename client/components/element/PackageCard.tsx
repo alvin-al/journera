2f9bc2fa4a0bde107d6f659f7eceacaca7791e61
alvvxal <alvvxal@gmail.com>
2024-11-05 07:59:55 +0000

    Modify card content

--- client/components/element/PackageCard.tsx
+++ client/components/element/PackageCard.tsx
@@ -1,15 +1,35 @@
 import Image from "next/image";
 
-type PackageCard  = {
-  title : String;
-  price : int;
-  duration : int;
-  maxPeople : int;
-  image : 
-}
+type PackageCard = {
+  title: string;
+  price: number;
+  duration: number;
+  maxPeople: number;
+  image: string;
+};
 
-const PackageCard = ({title, price, duration, maxPeople, image} : PackageCard) => {
-  return;
+const PackageCard = ({
+  title,
+  price,
+  duration,
+  maxPeople,
+  image,
+}: PackageCard) => {
+  return (
+    <div className="w-72 bg-gray-200 flex flex-col gap-2 h-full">
+      <div className="w-full h-48 overlay-hidden bg-yellow-200">
+        <Image src={image} width={300} height={200} alt={title} />
+      </div>
+      <p className="text-xl font-semibold">{title}</p>
+      <p className="text-lg flex flex-row gap-2">
+        <span className="text-gray-600">from </span>Rp. {price}
+      </p>
+      <div>
+        <p>{duration} Hari</p>
+        <p>{maxPeople} Orang</p>
+      </div>
+    </div>
+  );
 };
 
 export default PackageCard;
--- client/components/section/Package.tsx
+++ client/components/section/Package.tsx
@@ -1,6 +1,16 @@
+import PackageCard from "../element/PackageCard";
+
+const packageExample = {
+  title : "Journera Adventure 3",
+  price : 900000,
+  duration : 12,
+  maxPeople: 6,
+  image: "/image/malioboro.webp",
+}
+
 const Package = () => {
   return (
-    <div className="flex justify-center w-full h-screen">
+    <div className="flex justify-center w-full h-screen flex-col gap-24 mx-12">
       {/* Title */}
       <div className="flex flex-col items-center gap-8">
         <div className="mt-12 text-5xl">
@@ -11,7 +21,9 @@ const Package = () => {
         </div>
       </div>
       {/* Card */}
-      <div className=""></div>
+      <div className="">
+        <PackageCard title={packageExample.title} price={packageExample.price} duration={packageExample.duration} maxPeople={packageExample.maxPeople} image={packageExample.image} />
+      </div>
     </div>
   );
 };
