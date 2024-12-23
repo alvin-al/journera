b92d50e9cb09c1b54c5824cecfea69a994b57388
Alvin Al <m@Alvin-Mac.local>
2024-12-23 21:46:59 +0700

    Add about page

---
+++ components/element/AboutSection.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+import { PiAirplaneTilt } from "react-icons/pi";
+
+interface AboutSectionProps {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+  image: StaticImageData;
+  imageLeft?: boolean;
+}
+
+const AboutSection = ({
+  icon,
+  title,
+  description,
+  image,
+  imageLeft,
+}: AboutSectionProps) => {
+  return (
+    <div className='flex w-full h-full flex-col md:flex-row items-center'>
+      <div
+        className={`md:w-1/2 overflow-hidden flex justify-center items-center md:p-8 h-[400px] ${
+          imageLeft ? "md:order-2" : ""
+        }`}
+      >
+        <Image
+          src={image}
+          alt='Malioboro'
+          className='object-cover w-full h-full rounded-xl'
+          width={400}
+          height={400}
+        />
+      </div>
+      <div className='md:w-1/2 flex flex-col gap-6 p-8'>
+        <div className='text-5xl'>
+          <PiAirplaneTilt />
+        </div>
+        <div className='text-3xl'>{title}</div>
+        <div className='text-base text-gray-500'>{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
---
+++ components/pages/AboutUs.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import * as styles from "../../components/styles";
+import AboutSection from "@/components/element/AboutSection";
+import malioboro from "@/public/Hero.png";
+import Footer from "../section/Footer";
+import { PiAirplaneTilt } from "react-icons/pi";
+
+const aboutData = [
+  {
+    icon: "PiAirplaneTilt",
+    title: "Jelajahi Jogjakarta",
+    description:
+      "Journera membantu kamu menjelajahi Jogjakarta dengan mudah. Temukan destinasi impian di berbagai tempat. Buat perjalanan tak terlupakan bersama kami.",
+    image: malioboro,
+  },
+  {
+    icon: "PiAirplaneTilt",
+    title: "Kolaborasi Global",
+    description:
+      "Kami bekerja sama dengan mitra di seluruh dunia untuk memberikan pengalaman perjalanan terbaik. Nikmati keragaman budaya dan koneksi global.",
+    image: malioboro,
+    imageLeft: true,
+  },
+  {
+    icon: "PiAirplaneTilt",
+    title: "Pengalaman Istimewa",
+    description:
+      "Journera menciptakan perjalanan yang tak terlupakan. Dapatkan rekomendasi personalized dan layanan premium untuk pengalaman tak terlupakan.",
+    image: malioboro,
+  },
+];
+
+const AboutUs: React.FC = () => {
+  return (
+    <div>
+      <div
+        className={` flex flex-col ${styles.ContainerContentPadding} h-full mb-40`}
+      >
+        <div
+          className={`flex flex-col items-center gap-4 w-full justify-center py-12 mb-12`}
+        >
+          <div className='text-5xl'>About Us</div>
+          <div>
+            <p className='text-gray-500'>Lebih jauh mengenal Journera</p>
+          </div>
+        </div>
+        <div className='flex flex-col gap-12'>
+          {aboutData.map((data, index) => {
+            return (
+              <AboutSection
+                key={index}
+                icon={data.icon}
+                title={data.title}
+                description={data.description}
+                image={data.image}
+                imageLeft={data.imageLeft}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default AboutUs;
