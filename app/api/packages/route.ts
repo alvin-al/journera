15041a54b1f1ff5adeeefcbbed56b85186958ce7
Alvin Al <alvinstuff@Alvin-Mac.local>
2024-12-25 14:11:34 +0700

    Change routes code

--- app/api/packages/route.ts
+++ app/api/packages/route.ts
@@ -3,10 +3,29 @@ import prisma from "@/lib/prisma";
 
 export async function GET() {
   try {
-    const packages = await prisma.package.findMany();
+    const packages = await prisma.package.findMany({
+      select: {
+        id: true,
+        name: true,
+        featuredImage: true,
+        itinerary: true,
+        price: true,
+        originalPrice: true,
+        slug: true,
+        description: true,
+      },
+    });
+
+    if (!packages) {
+      return NextResponse.json({ error: "No packages found" }, { status: 404 });
+    }
+
     return NextResponse.json(packages);
   } catch (error) {
     console.error("Error fetching packages:", error);
-    return NextResponse.error();
+    return NextResponse.json(
+      { error: "Failed to fetch packages" },
+      { status: 500 }
+    );
   }
 }
