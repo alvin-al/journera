391f1776ab3426ec0e8877a70d5a0889686dfd02
Alvin Al <alvinstuff@Alvin-Mac.local>
2024-12-27 05:55:40 +0700

    Add types folder

---
+++ types/packages.ts
@@ -0,0 +1,10 @@
+export interface PackageProps {
+  id: string;
+  name: string;
+  featuredImage: string;
+  itinerary: object;
+  price: string;
+  originalPrice: number;
+  slug: string;
+  description: string;
+}
